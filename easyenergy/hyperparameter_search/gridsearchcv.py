63b93e6035ab0c079e39987133326ad883c90ecb
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-07-16 03:00:44 +0530

    Create gridsearchcv.py

---
+++ easyenergy/hyperparameter_search/gridsearchcv.py
@@ -0,0 +1,19 @@
+from sklearn.model_selection import GridSearchCV
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+
+from codecarbon import EmissionsTracker
+
+def search(model_fn, x_train,y_train,x_test,y_test,params,epochs=20, project_name='grid_search'):
+    model = KerasClassifier(build_fn=model_fn, epochs=epochs)
+    param_grid = params
+    grid = GridSearchCV(estimator=model, param_grid=param_grid)
+
+    tracker = EmissionsTracker(project_name=project_name)
+    tracker.start()
+    grid_result = grid.fit(x_train, y_train)
+    emissions = tracker.stop()
+
+    print(f"Best Accuracy : {grid_result.best_score_} using {grid_result.best_params_}")
+    print(f"Emissions : {emissions} kg CO₂")
+    
+    return grid_result.best_params_, emissions
