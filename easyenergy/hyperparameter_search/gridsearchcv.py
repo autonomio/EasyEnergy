9b02163b94e981e4662d5be5a63e3eb28512e837
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-07-17 01:33:44 +0530

    update epochs for KerasClassifier in gridsearch

--- easyenergy/hyperparameter_search/gridsearchcv.py
+++ easyenergy/hyperparameter_search/gridsearchcv.py
@@ -1,9 +1,16 @@
 from sklearn.model_selection import GridSearchCV
 from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
-
 from codecarbon import EmissionsTracker
 
-def search(model_fn, x_train,y_train,x_test,y_test,params,epochs=20, project_name='grid_search'):
+
+def search(model_fn, x_train, y_train, params, epochs=None,
+           project_name='grid_search'):
+
+    if epochs is None:
+        model = KerasClassifier(build_fn=model_fn)
+    else:
+        model = KerasClassifier(build_fn=model_fn, epochs=epochs)
+
     model = KerasClassifier(build_fn=model_fn, epochs=epochs)
     param_grid = params
     grid = GridSearchCV(estimator=model, param_grid=param_grid)
@@ -13,7 +20,8 @@ def search(model_fn, x_train,y_train,x_test,y_test,params,epochs=20, project_nam
     grid_result = grid.fit(x_train, y_train)
     emissions = tracker.stop()
 
-    print(f"Best Accuracy : {grid_result.best_score_} using {grid_result.best_params_}")
+    print(f'''Best Accuracy : {grid_result.best_score_}
+          using {grid_result.best_params_}''')
     print(f"Emissions : {emissions} kg CO₂")
-    
+
     return grid_result.best_params_, emissions
