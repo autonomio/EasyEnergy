315ac25eecf3ec3aab1a540b60df2b5ec8de550a
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-07-19 16:54:44 +0530

    add __init__.py, setup.py and update .gitignore

---
+++ setup.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2022 Autonomio
+import setuptools
+
+
+DESCRIPTION = 'Energy tracking for your machine learning experiments'
+LONG_DESCRIPTION = '''\
+EasyEnergy makes it easier to track energy usage for various machine learning
+cycles, including model training, model inference and hyperparameter tuning.
+'''
+
+DISTNAME = 'EasyEnergy'
+MAINTAINER = 'Mikko Kotila'
+MAINTAINER_EMAIL = 'mailme@mikkokotila.com'
+URL = 'http://autonom.io'
+LICENSE = 'MIT'
+DOWNLOAD_URL = 'https://github.com/autonomio/EasyEnergy/'
+
+VERSION = '0.0.1'
+
+
+install_requires = ['pyrapl',
+                    'codecarbon']
+
+
+setuptools.setup(
+    name=DISTNAME,
+    version=VERSION,
+    author=MAINTAINER,
+    author_email=MAINTAINER_EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license=LICENSE,
+    url=DOWNLOAD_URL,
+    install_requires=install_requires,
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    include_package_data=True
+)
