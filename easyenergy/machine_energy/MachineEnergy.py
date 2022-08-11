5ec4df20dc3d55b86f3015606af188e1d3d15dde
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-08-12 01:33:47 +0530

    Add MachineEnergy class

---
+++ easyenergy/machine_energy/MachineEnergy.py
@@ -0,0 +1,15 @@
+import json
+
+
+class MachineEnergy:
+    def __init__(self, config):
+        # Handle the case when `config` is a filepath
+        if isinstance(config, str):
+            with open(config, 'r') as f:
+                self.config_data = json.load(f)
+
+        # Handle the case when `config` is dict
+        elif isinstance(config, dict):
+            self.config_data = config
+            with open('config.json', 'w') as outfile:
+                json.dump(self.config_data, outfile, indent=2)
