45a3346319061bc960b9a40bbc23755e3ade9d35
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-08-13 19:41:20 +0530

    add mnist_keras to models.py

--- easyenergy/machine_energy/models.py
+++ easyenergy/machine_energy/models.py
@@ -50,3 +50,30 @@ def mnist_pl(self):
     # Train the model ⚡
     trainer.fit(mnist_model, train_loader)
     return self
+
+
+def mnist_keras(self):
+    from easyenergy.callbacks.keras import TrainCallback
+    import tensorflow as tf
+
+    mnist = tf.keras.datasets.mnist
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    model = tf.keras.models.Sequential(
+        [
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(10),
+        ]
+    )
+
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
+
+    cb = TrainCallback()
+    model.fit(x_train, y_train, epochs=3, callbacks=[cb])
+    return self
