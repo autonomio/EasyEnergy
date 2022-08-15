767c1c9ef830a419bb18867e350a805e3a944c44
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-08-16 00:18:09 +0530

    Update tracker run steps

--- easyenergy/machine_energy/tracker_run.py
+++ easyenergy/machine_energy/tracker_run.py
@@ -1,6 +1,18 @@
-from .utils import ssh_connect
+from .utils import ssh_connect, ssh_file_transfer, ssh_run
+import threading
 
 
 def tracker_run(self):
+
     clients = ssh_connect(self)
-    
+    threads = []
+    for machine_id, client in clients.items():
+        ssh_file_transfer(self, client, machine_id)
+
+        args = (self, client, machine_id)
+        thread = threading.Thread(target=ssh_run, args=args)
+        thread.start()
+        threads.append(thread)
+
+    for t in threads:
+        t.join()
