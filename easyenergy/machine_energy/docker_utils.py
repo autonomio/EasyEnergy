71cab2bb363d39be2dff972de287d830cd724553
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-08-20 00:59:03 +0530

    add write dockerfile method

--- easyenergy/machine_energy/docker_utils.py
+++ easyenergy/machine_energy/docker_utils.py
@@ -9,9 +9,46 @@ def docker_install_commands(self):
 
 def write_shell_script(self):
     '''write docker commands to shell script'''
-    commands = docker_install_commands(self)
 
-    with open('/tmp/{}/jako_docker.sh'.format(
-            self.experiment_name), 'w') as f:
+    commands = docker_install_commands(self)
+    experiment_name = self.experiment_name
+
+    with open('/tmp/{}/easyenergy_docker.sh'.format(
+            experiment_name), 'w') as f:
+
+        for command in commands:
+            f.write(command + '\n')
+
+
+def write_dockerfile(self):
+    framework = self.framework
+    experiment_name = self.experiment_name
+
+    if framework == 'keras':
+        filename = 'easyenergy_mnist_keras.py'
+
+    elif framework == 'pl':
+        filename = 'easyenergy_mnist_pl.py'
+
+    else:
+        filename = 'easyenergy_custom_model.py'
+
+    commands = ['FROM abhijithneilabraham/easyenergy_docker_image',
+
+                'RUN mkdir -p /tmp/',
+
+                'COPY {} /tmp/{}'.format(filename, filename),
+
+
+                'COPY easyenergy_config.json /tmp/easyenergy_config.json',
+
+                'CMD python3 /tmp/{}'.format(filename),
+
+                'RUN chmod -R 777 /tmp/'
+                ]
+
+    with open('/tmp/{}/Dockerfile'.format(
+            experiment_name), 'w') as f:
+
         for command in commands:
             f.write(command + '\n')
