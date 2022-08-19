5d17f02ff68b30f0925420e34c6dca5e87f24f87
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-08-20 01:03:49 +0530

    add docker_ssh_file_transfer

--- easyenergy/machine_energy/docker_utils.py
+++ easyenergy/machine_energy/docker_utils.py
@@ -1,3 +1,6 @@
+import os
+
+
 def docker_install_commands(self):
     '''commands to install docker in a machine'''
 
@@ -52,3 +55,30 @@ def write_dockerfile(self):
 
         for command in commands:
             f.write(command + '\n')
+
+
+def docker_ssh_file_transfer(self, client):
+    '''Transfer the docker scripts to the remote machines'''
+
+    experiment_name = self.experiment_name
+    dest_dir = self.dest_dir
+
+    write_dockerfile(self)
+
+    sftp = client.open_sftp()
+
+    try:
+        sftp.chdir(self.dest_dir)  # Test if dest dir exists
+
+    except IOError:
+        sftp.mkdir(self.dest_dir)  # Create dest dir
+        sftp.chdir(self.dest_dir)
+
+    docker_files = ['easyenergy_docker.sh', 'Dockerfile']
+
+    for file in os.listdir("/tmp/{}".format(experiment_name)):
+        if file in docker_files:
+            sftp.put("/tmp/{}/".format(
+                experiment_name) + file, dest_dir + file)
+
+    sftp.close()
