9dac8167782a0e3f0de577c6427314d861389531
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-09-01 20:00:51 +0530

    Update test_machine_energy.py

--- tests/test_machine_energy.py
+++ tests/test_machine_energy.py
@@ -8,4 +8,7 @@ shutil.copy('../autonomio-dev.pem', 'autonomio-dev.pem')
 with open('../config.json', 'r') as f:
     config = json.load(f)
 
-me = MachineEnergy(config)
+def test_machine_energy():
+    me = MachineEnergy(config)
+
+test_machine_energy()
